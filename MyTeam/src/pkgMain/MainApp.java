75878120a447aa6aa2a4be60bc6ac525109b1529
Riccardo <77676323+bergerr1@users.noreply.github.com>
2021-06-18 11:21:01 +0200

    Update MainApp.java

--- MyTeam/src/pkgMain/MainApp.java
+++ MyTeam/src/pkgMain/MainApp.java
@@ -6,6 +6,7 @@ public class MainApp {
 	public static void main(String[] args) {
 		Player player = new Player(2, "Dragovic", "Defense");
 		System.out.println("==" + player);
-		
+		Player player2 = new Player(4, "Hinteregger", "Defense");
+		System.out.println("==" + player2);
 	}
 }
