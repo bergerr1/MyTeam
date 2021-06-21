b428ce31814ae1162f338f1b0d28c23652cc2f56
Riccardo <77676323+bergerr1@users.noreply.github.com>
2021-06-21 15:45:18 +0200

    changed name

--- MyTeam/src/pkgMain/MainApp.java
+++ MyTeam/src/pkgMain/MainApp.java
@@ -6,7 +6,7 @@ public class MainApp {
 	public static void main(String[] args) {
 		Player player = new Player(2, "Dragovic", "Defense");
 		System.out.println("==" + player);
-		Player player2 = new Player(4, "Hinteregger", "Defense");
+		Player player2 = new Player(4, "Bratan", "Defense");
 		System.out.println("==" + player2);
 	
 		player = new Player(8, "Baumgartner", "thomas");
